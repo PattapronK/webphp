0ec3c8d013324f84296ea222d460a0c92161af4b
Pattapron1 <pattapron.keaw@gmail.com>
2024-02-02 16:25:44 +0700

    ui and show display a table when a button is clicked

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="web.php">
+</head>
+<body>
+     <!-- topbay -->
+     <ul id="topbar">
+        <a>Company name</a>
+        <input id="Search" style="background-color: #f5f2f2dd;" type="text" placeholder="Search..." name="search" > 
+        <li id="singout"  style="float:right;">
+            <a href="#singout">Singout</a>
+        </li>
+    </ul>
+
+
+    <!-- img-->
+   <!-- <div class="boxP" style="float:right;">
+        <a href="#sh">Share</a>
+        <span>
+        <a href="#e" >Export</a>
+        </span>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#w" padding:10px;>This week</a>
+   </div> -->
+     <h1>Data</h1>
+     <!-- <div class="table">
+        <h1>table databases</h1>
+     </div> -->
+     <!-- mg src="pic/1706514246929@2x.jpg" alt=""> -->
+      
+
+     <!-- Sidebar -->
+     <div class="Sidebar">
+        <ul>
+            <li><a href="web.php" style="color: #007bff;">Show data</a></li>
+            <li><a href="#Order">Add data</a></li>
+            <li><a href="#Products">------</a></li>
+            <li><a href="#Customer">-------r</a></li>
+      
+          </ul>
+     </div>
+</body>
+</html>
--- web.php
+++ web.php
@@ -1,4 +1,79 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>webphp</title>
+  <link rel="stylesheet" href="index.css">
+</head>
+<body>
+
 <?php
-$txt = "PHP";
-echo "I love $txt!";
+$servername = "localhost";
+$username = "root";
+$password = "asdf1234**";
+$database = "majoret";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM production";
+$result = mysqli_query($conn, $sql);
+
+// Check query
+if (!$result) {
+    die("Query failed: " . mysqli_error($conn));
+}
 ?>
+
+<div class="table">
+<?php
+echo "<table>";
+echo "<tr>
+      <th>Production Line</th>
+      <th>ID</th>
+      <th>Item_Number</th>
+      <th>Description</th>
+      <th>Production_Rate</th>
+      <th>Rate_Hours</th>
+      <th>Quantity_Ordered</th>
+      <th>Quantity_Completed</th>
+      <th>Qty_Open</th>
+      <th>Order_Date</th>
+      <th>Release_Date</th>
+      <th>Due_Date</th>
+      <th>Sales_Job</th>
+      <th>WO_Stat</th>
+      </tr>";
+
+while ($row = mysqli_fetch_assoc($result)) {
+  echo "<tr>";
+  echo "<td>" . $row["Production_Line"] . "</td>";
+  echo "<td>" . $row["ID"] . "</td>";
+  echo "<td>" . $row["Item_Number"] . "</td>";
+  echo "<td>" . $row["Description"] . "</td>";
+  echo "<td>" . $row["Production_Rate"] . "</td>";
+  echo "<td>" . $row["Rate_Hours"] . "</td>";
+  echo "<td>" . $row["Quantity_Ordered"] . "</td>";
+  echo "<td>" . $row["Quantity_Completed"] . "</td>";
+  echo "<td>" . $row["Qty_Open"] . "</td>";
+  echo "<td>" . $row["Order_Date"] . "</td>";
+  echo "<td>" . $row["Release_Date"] . "</td>";
+  echo "<td>" . $row["Due_Date"] . "</td>";
+  echo "<td>" . $row["Sales_Job"] . "</td>";
+  echo "<td>" . $row["WO_Stat"] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+mysqli_close($conn);
+?>
+</div>
+
+
+</body>
+</html>
